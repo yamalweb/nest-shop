01775b2a56bee2417647158607af04204d929939
yamalweb <o.starigin@gmail.com>
2024-04-30 18:59:11 +0300

    first

---
+++ src/review/review.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ReviewModel } from './review.model';
+
+@Controller('review')
+export class ReviewController {
+  @Post('create')
+  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+
+  @Get('byProduct/:productId')
+  async getByProduct(@Param('productId') productId: string) {}
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
+}
