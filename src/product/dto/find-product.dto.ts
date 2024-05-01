db9e4e2203061866035cea2834350758a24a3e0f
yamalweb <o.starigin@gmail.com>
2024-05-01 10:48:07 +0300

    controller complete

--- src/product/dto/find-product.dto.ts
+++ src/product/dto/find-product.dto.ts
@@ -1,4 +1,5 @@
-export class FindProductDto {
-  category: string;
-  limit: number;
+import { TopLevelCategory } from 'src/top-page/top-page.model';
+
+export class FindTopPageDto {
+  firstCategory: TopLevelCategory;
 }
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -9,14 +9,20 @@ import {
   Post,
 } from '@nestjs/common';
 import { TopPageModel } from './top-page.model';
+import { FindTopPageDto } from './dto/find-top-page.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
-  @Get(':id')
-  async get(@Param('id') id: string) {}
+  @Get('get/:alias')
+  async get(@Param('alias') alias: Promise<TopPageModel>) {
+    return this.configService.get('TEST');
+  }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {}
@@ -26,5 +32,5 @@ export class TopPageController {
 
   @HttpCode(200)
   @Post()
-  async find(@Body() dto: FindProductDto) {}
+  async find(@Body() dto: FindTopPageDto) {}
 }
